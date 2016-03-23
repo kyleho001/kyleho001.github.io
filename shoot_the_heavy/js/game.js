7c42418656ff01d611e3cc9f42396b3c2bb501b9
kyleho001 <kyleho@kylesGamingMac.cais.edu.hk>
2016-03-23 13:43:28 +0800

    lel

--- shoot_the_heavy/js/game.js
+++ shoot_the_heavy/js/game.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   //generates Heavy every 3 seconds
   setInterval(function(){ 
     $("#heavy").toggleClass("newclass");
-  }, 3000);
+  }, 900;
   
   //click to kill = replace Heavy w/ explosion gif
     $("#heavy").click(function(){
