1bab70fcee98a2e11631dd3098f6b06d8cae550f
kyleho001 <kyleho@kylesGamingMac.cais.edu.hk>
2016-03-23 14:35:12 +0800

    lel
    added sounds

--- shoot_the_heavy/js/game.js
+++ shoot_the_heavy/js/game.js
@@ -3,22 +3,29 @@ $(document).ready(function(){
   //generates Heavy every 3 seconds
   setInterval(function(){ 
     $("#heavy").toggleClass("newclass");
-  }, 3000;
-  
+  }, 2000);
+
   //click to kill = replace Heavy w/ explosion gif
     $("#heavy").click(function(){
+
+      var audio = $("#clickSound")[0];
+      $("#heavy").click(function() {
+       audio.play();
+      });
+
     	$("#heavy").css("background", "url('http://1.bp.blogspot.com/-EXrHTPc2OXk/Uv_cGFw56gI/AAAAAAAAAKs/Z-OHd1qeaU0/s1600/kevinexplosionlarge.gif')");
+
+    setTimeout(
+      function() {
+        $("#heavy").show();
+      }, 2500);     
+
 
     	setTimeout(
   		function() {
     		$("#heavy").hide();
     		$("#heavy").css("background", "url('http://c3e308.medialib.glogster.com/media/3a/3a4d62b8905738633b6dc2db7f18f5e6a3cf19b8905024e29ae3deb1f7fe1a06/tf2-heavy-3-png.png')");
   		}, 900);
-
-		setTimeout(
-  		function() {
-			$("#heavy").show();
-  		}, 2000);    	
 
     });
 
