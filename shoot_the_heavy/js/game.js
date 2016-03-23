c3fabb6aa63b9062fc2e6917de5c6ec38fda5971
kyleho001 <kyleho@kylesGamingMac.cais.edu.hk>
2016-03-23 11:22:28 +0800

    lel

--- shoot_the_heavy/js/game.js
+++ shoot_the_heavy/js/game.js
@@ -7,10 +7,18 @@ $(document).ready(function(){
   
   //click to kill = replace Heavy w/ explosion gif
     $("#heavy").click(function(){
-    	//replace with explosion gif
-    	$("#heavy").toggleClass("explosion");
+    	$("#heavy").css("background", "url('http://1.bp.blogspot.com/-EXrHTPc2OXk/Uv_cGFw56gI/AAAAAAAAAKs/Z-OHd1qeaU0/s1600/kevinexplosionlarge.gif')");
 
-    	//hide self
+    	setTimeout(
+  		function() {
+    		$("#heavy").hide();
+    		$("#heavy").css("background", "url('http://c3e308.medialib.glogster.com/media/3a/3a4d62b8905738633b6dc2db7f18f5e6a3cf19b8905024e29ae3deb1f7fe1a06/tf2-heavy-3-png.png')");
+  		}, 900);
+
+		setTimeout(
+  		function() {
+			$("#heavy").show();
+  		}, 2000);    	
 
     });
 
