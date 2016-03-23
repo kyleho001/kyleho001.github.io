be6e86ed37674113e6c185c5734856108bb1cf3c
kyleho001 <kyleho@kylesGamingMac.cais.edu.hk>
2016-03-23 15:13:04 +0800

    pootis

--- shoot_the_heavy/js/game.js
+++ shoot_the_heavy/js/game.js
@@ -7,25 +7,21 @@ $(document).ready(function(){
 
   //click to kill = replace Heavy w/ explosion gif
     $("#heavy").click(function(){
-
       var audio = $("#clickSound")[0];
-      $("#heavy").click(function() {
-       audio.play();
-      });
+      audio.play();
 
     	$("#heavy").css("background", "url('http://1.bp.blogspot.com/-EXrHTPc2OXk/Uv_cGFw56gI/AAAAAAAAAKs/Z-OHd1qeaU0/s1600/kevinexplosionlarge.gif')");
 
-    setTimeout(
-      function() {
-        $("#heavy").show();
-      }, 2500);     
-
-
     	setTimeout(
-  		function() {
-    		$("#heavy").hide();
-    		$("#heavy").css("background", "url('http://c3e308.medialib.glogster.com/media/3a/3a4d62b8905738633b6dc2db7f18f5e6a3cf19b8905024e29ae3deb1f7fe1a06/tf2-heavy-3-png.png')");
-  		}, 900);
+  		  function() {
+    		  $("#heavy").hide();
+    		  $("#heavy").css("background", "url('http://c3e308.medialib.glogster.com/media/3a/3a4d62b8905738633b6dc2db7f18f5e6a3cf19b8905024e29ae3deb1f7fe1a06/tf2-heavy-3-png.png')");
+  		  }, 900);
+
+      setTimeout(
+        function() {
+          $("#heavy").show();
+        }, 1500);
 
     });
 
