b543dd8bcb32b9d65a7001ad7bdbc03ba40a9c54
kyleho001 <kyleho@kylesGamingMac.cais.edu.hk>
2016-03-24 11:21:36 +0800

    lel

--- shoot_the_heavy/js/game.js
+++ shoot_the_heavy/js/game.js
@@ -25,4 +25,37 @@ $(document).ready(function(){
 
     });
 
+      function collision($div1, $div2) {
+        var x1 = $div1.offset().left;
+        var y1 = $div1.offset().top;
+        var h1 = $div1.outerHeight(true);
+        var w1 = $div1.outerWidth(true);
+        var b1 = y1 + h1;
+        var r1 = x1 + w1;
+        var x2 = $div2.offset().left;
+        var y2 = $div2.offset().top;
+        var h2 = $div2.outerHeight(true);
+        var w2 = $div2.outerWidth(true);
+        var b2 = y2 + h2;
+        var r2 = x2 + w2;
+        
+      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) {
+        //if it is not colliding, do nothing
+        //$("#heavy").css("background", "yellow");
+        return false;
+      }
+      else {
+        //if it is colliding, wait 1 sec, then switch to loser page
+        setTimeout(function(){
+          window.location.href = "http://kyleho001.github.io/shoot_the_heavy/lose/lose.html";
+        }, 1000); 
+        //$("#heavy").css("background", "green");
+        return true;
+      }
+    }
+
+    window.setInterval(function() {
+        $('#result').text(collision($('#scout'), $('#heavy')));
+    }, 200);
+
 });
